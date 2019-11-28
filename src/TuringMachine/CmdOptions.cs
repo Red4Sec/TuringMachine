87368d638b10d24b22e342de4417fe103cdfbccf
Shargon <shargon@gmail.com>
2019-11-28 12:46:58 +0100

    Format

--- src/TuringMachine/CmdOptions.cs
+++ src/TuringMachine/CmdOptions.cs
@@ -5,43 +5,43 @@ using TuringMachine.Core;
 
 namespace TuringMachine
 {
-    [Verb("listen", HelpText = "Listen server")]
-    public class CmdOptions : CommandLineOptions
-    {
-        [Option('u', "onlyUniques", Required = false, HelpText = "Store only unique errors.")]
-        public bool OnlyUniques { get; set; } = true;
+	[Verb("listen", HelpText = "Listen server")]
+	public class CmdOptions : CommandLineOptions
+	{
+		[Option('u', "onlyUniques", Required = false, HelpText = "Store only unique errors.")]
+		public bool OnlyUniques { get; set; } = true;
 
-        [Option('i', "inputs", Required = false, HelpText = "Set input folder.")]
-        public IEnumerable<string> Inputs { get; set; }
+		[Option('i', "inputs", Required = false, HelpText = "Set input folder.")]
+		public IEnumerable<string> Inputs { get; set; }
 
-        [Option('c', "configs", Required = false, HelpText = "Set config folder.")]
-        public IEnumerable<string> Configs { get; set; }
+		[Option('c', "configs", Required = false, HelpText = "Set config folder.")]
+		public IEnumerable<string> Configs { get; set; }
 
-        [Option('r', "rpc", Required = false, HelpText = "Rpc EndPoint (127.0.0.1,1234)")]
-        public string RpcEndPoint { get; set; }
+		[Option('r', "rpc", Required = false, HelpText = "Rpc EndPoint (127.0.0.1,1234)")]
+		public string RpcEndPoint { get; set; }
 
-        /// <summary>
-        /// Get files
-        /// </summary>
-        /// <param name="inputs">Inputs</param>
-        /// <returns>Files</returns>
-        public static IEnumerable<string> GetFiles(IEnumerable<string> inputs)
-        {
-            foreach (var entry in inputs)
-            {
-                if (File.Exists(entry))
-                {
-                    yield return entry;
-                }
+		/// <summary>
+		/// Get files
+		/// </summary>
+		/// <param name="inputs">Inputs</param>
+		/// <returns>Files</returns>
+		public static IEnumerable<string> GetFiles(IEnumerable<string> inputs)
+		{
+			foreach (var entry in inputs)
+			{
+				if (File.Exists(entry))
+				{
+					yield return entry;
+				}
 
-                if (Directory.Exists(entry))
-                {
-                    foreach (var file in Directory.GetFiles(entry))
-                    {
-                        yield return file;
-                    }
-                }
-            }
-        }
-    }
+				if (Directory.Exists(entry))
+				{
+					foreach (var file in Directory.GetFiles(entry))
+					{
+						yield return file;
+					}
+				}
+			}
+		}
+	}
 }
